0ece59615870d0cb26776df5d0ca56a1bd37cdb4
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-01 16:48:25 +0100

    Add project files.

---
+++ Exemplo_ASP_NET_Core_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Exemplo_ASP_NET_Core_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
