48271214fee93f5d77bab06fb8207ea567911b71
https://github.com/RickyMartin12/moodle_elearning.git <11knum15@gmail.com>
2022-04-06 17:49:11 +0100

    first commit

---
+++ Exemplo_ASP_NET_Core_MVC/Controllers/AppointamentController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exemplo_ASP_NET_Core_MVC.Controllers
+{
+    public class AppointamentController : Controller
+    {
+        public IActionResult Index()
+        {
+            string today = DateTime.Now.ToShortDateString();
+            return Ok(today);
+            //return View();
+        }
+    }
+}
